2631f9a383352f435698843321b7fe8cf9550853
Ian Stewart <90149902+IanStewart2001@users.noreply.github.com>
2025-07-03 17:20:07 -0400

    Created basic power spectrum plot...needs refining with SR/CF measurements

--- gui/widgets/power_spectrum.cpp
+++ gui/widgets/power_spectrum.cpp
@@ -1,4 +1,33 @@
 #include "power_spectrum.h"
 
-power_spectrum::power_spectrum(QWidget* parent, const std::vector<std::complex<float> > &signal) : QWidget(parent) {
+power_spectrum::power_spectrum(QWidget* parent, const std::vector<std::complex<float> > &data, long int sample_rate, float center_frequency) : QWidget(parent) {
+    
+
+    //general plot gets created...still needs corrected taking into account sample rate, CF, etc. 
+
+    move(400,0);
+    setMinimumSize(450, 250);
+    setWindowFlags(Qt::Window);
+
+    spectrum_plot = new QCustomPlot(this);
+    spectrum_plot->addGraph();
+    layout = new QVBoxLayout();
+
+    KissFFT fftout = KissFFT(1024 * 5);
+    fft_data = fftout.fft(data);
+    for (unsigned long i = 0; i < fft_data.size(); i++) {
+        float power = std::norm(fft_data.at(i)); // magnitude squared
+        float power_dB = 10.0f * std::log10(power + 1e-12f); // add epsilon to avoid log(0)
+        spectrum_plot->graph(0)->addData(i, power_dB);
+    }
+
+    layout->addWidget(spectrum_plot);
+    setLayout(layout);
+
+    //Makes graph interactive
+    spectrum_plot->setInteraction(QCP::iRangeDrag, true);
+    spectrum_plot->setInteraction(QCP::iRangeZoom, true);
+    spectrum_plot->setInteraction(QCP::iSelectPlottables, true);
+
+
 };
--- gui/widgets/power_spectrum.h
+++ gui/widgets/power_spectrum.h
@@ -1,10 +1,19 @@
 #pragma once
+#include "qcustomplot.h"
+#include "kiss_fft_wrapper.h"
 #include <QWidget>
+#include <complex>
+#include <vector>
+#include <QVBoxLayout>
 
 class power_spectrum : public QWidget
 {
     Q_OBJECT
 
     public:
-    explicit power_spectrum(QWidget* parent = nullptr, const std::vector<std::complex<float> > &signal = {});
+        explicit power_spectrum(QWidget* parent = nullptr, const std::vector<std::complex<float> > &signal = {}, long int sample_rate = 0, float center_frequency = 0);
+        QCustomPlot* spectrum_plot;
+        QVBoxLayout* layout;
+        KissFFT* fftout;
+        std::vector<std::complex<float> > fft_data;
 };
